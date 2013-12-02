29891ad9ed3b0343afbb772269834bed905eb824
Tyler Green <tyler.green2@gmail.com>
2013-12-02 16:42:41 -0600

    Generalized type signature of splitSection

--- src/main/scala/Parser.scala
+++ src/main/scala/Parser.scala
@@ -1,6 +1,28 @@
 package com.tyler.sensorCheck
+import scala.annotation.tailrec
+
 
 class Parser(input : Array[String]){
   val referenceHumidity = input(0).split(' ')(2).toDouble
   val referenceTemperature = input(0).split(' ')(1).toDouble
+
+  val sensorReadings = Array()
+
+
+  def splitSections (
+    isSeparator: Any => Boolean,
+    list: List[Any],
+    accum: List[List[Any]]
+  ) : List[List[Any]] = {
+    if (list == Nil) {
+      accum.reverse
+    }
+    else {
+      val header = list.head
+      val rest = list.tail
+      val (section, residue) = rest.span(x => !isSeparator(x))
+      splitSections(isSeparator, residue,  (header :: section) :: accum )
+    }
+  }
+  
 }
--- src/test/scala/ParserSpec.scala
+++ src/test/scala/ParserSpec.scala
@@ -16,7 +16,35 @@ class ParserSpec extends FlatSpec {
     assertResult(70.0){
       parser.referenceTemperature
     }
-
   }
+
+  it should "be able to split a list into sections" in {
+    val input = List("a", "1", "2", "a", "3", "4", "a", "5", "6")
+    val result = List(List("a", "1", "2"), List("a", "3", "4"), List("a", "5", "6"))
+    assert(result === parser.splitSections({_ == "a"}, input, Nil))
+
+    val input2 = List(100, 1, 2, 100, 3, 4, 100, 5, 6)
+    val result2 = List(List(100, 1, 2), List(100, 3, 4), List(100, 5, 6))
+    assert(result2 === parser.splitSections({_ == 100}, input2, Nil))
+  }
+
+
+  // it should "separate the readings by sensor" in {
+  //   assertResult(4){
+  //     parser.sensorReadings.length
+  //   }
+  //   assertResult("thermometer temp-1") {
+  //     parser.sensorReadings(0)(0)
+  //   }
+  //   assertResult("thermometer temp-2") {
+  //     parser.sensorReadings(1)(0)
+  //   }
+  //   assertResult("thermometer temp-2") {
+  //     parser.sensorReadings(2)(0)
+  //   }
+  //   assertResult("humidity hum-2") {
+  //     parser.sensorReadings(3)(0)
+  //   }
+  // }
 
 }
