52fbdf0a1a98080bf5fec9a545b0a20eb70c3268
Tyler Green <tyler.green2@gmail.com>
2013-12-05 14:11:35 -0600

    added a main method that reads from stdin until EOF

--- src/main/scala/SensorCheck.scala
+++ src/main/scala/SensorCheck.scala
@@ -1,10 +1,28 @@
 package com.tyler.sensorCheck
+import com.tyler.sensorCheck.Input
+import com.tyler.sensorCheck.Parser
+import com.tyler.sensorCheck.Sensor
 
-class Complex(real: Double, imaginary: Double) {
-  def re = real
-  def im = imaginary
+object SensorCheck{
+  def main(args: Array[String]){
+    println("start")
+    var lines = io.Source.stdin.getLines
+    //var sensorLog = lines.mkString("\n")
+    var parser = new Parser(lines.toArray) //change parser to take an iterator
+    var output = parser.sensors.map(x => x.name + ": " + x.classify)
+    for (ln <- output) println(ln)
+  }
+  // def checkReadings(
+  //   referenceTemperature : Double,
+  //   referenceHumidity : Double,
+  //   readings : Array[Map[String, String]]){
+  //   readings.map(x => x("header") match {
+  //     case Array("humidity", sensorName) => sensorName + ": " + HygrometerCheck.classify(referenceHumidity, x._2)
+  //     case Array("temperature", sensorName) => sensorName + ": " + ThermometerCheck.classify(referenceTemperature, x._2)
+  //   }
+  //   )
+  // }
 }
 
-object Monkey {
-  var arm = "monk-arm"
-}
+
+
