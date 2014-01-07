42b55e30ad090d045e5cc858c3bee46b375e9d19
Tyler Green <tyler.green2@gmail.com>
2014-01-07 16:59:50 -0600

    added Eof case class

--- src/main/scala/LineParser.scala
+++ src/main/scala/LineParser.scala
@@ -12,6 +12,7 @@ case class Reference(temperature: Double, humidity: Double) extends InputLine
 case class SensorDeclaration(sensor: SensorType) extends InputLine
 case class Reading(sensorName: String, time: TimeStamp, quantity: Double) extends InputLine
 case class Unknown(line: String) extends InputLine
+case class Eof() extends InputLine
 
 class PrimitiveParser extends RegexParsers {
   def number: Parser[Double] = """\d+(\.\d*)?""".r ^^ { _.toDouble }
