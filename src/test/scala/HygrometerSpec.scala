cb5d62be39b8b3b52f46956dea58f07be557b0e0
Tyler Green <tyler.green2@gmail.com>
2014-01-13 13:20:43 -0600

    updated tests

--- src/test/scala/HygrometerSpec.scala
+++ src/test/scala/HygrometerSpec.scala
@@ -1,9 +1,11 @@
 import org.scalatest.FlatSpec
 import com.tyler.sensorCheck.Hygrometer
+import com.tyler.sensorCheck.Ok
+import com.tyler.sensorCheck.Discard
 
 class HygrometerSpec extends FlatSpec {
   "A Hygrometer" should "return 'ok' if all the readings are within 1% of the reference" in {
-    assertResult("hum-1: OK"){
+    assertResult(Ok()){
       val h = new Hygrometer("hum-1", 45)
       h.addSample(44.1)
       h.addSample(45.9)
@@ -11,12 +13,12 @@ class HygrometerSpec extends FlatSpec {
     }
   }
   it should "return 'discard' if any of the readings are not within 1% of the reference" in {
-    assertResult("hum-1: discard"){
+    assertResult(Discard()){
       val h = new Hygrometer("hum-1", 45)
       h.addSample(46)
       h.classify
     }
-    assertResult("hum-1: discard"){
+    assertResult(Discard()){
       val h = new Hygrometer("hum-1", 45)
       h.addSample(45)
       h.addSample(45)
--- src/test/scala/ThermometerSpec.scala
+++ src/test/scala/ThermometerSpec.scala
@@ -5,7 +5,7 @@ import com.tyler.sensorCheck._
 class ThermometerSpec extends FlatSpec {
 
   "A Thermometer" should "return 'ultra precise' if the mean of the readings is within 0.5 degrees of the known temperature and the std deviation is < 3" in {
-    assertResult("temp-1: ultra precise"){
+    assertResult(UltraPrecise()){
       val check = new Thermometer("temp-1", 70)
       check.addSample(70)
       check.addSample(70)
@@ -14,7 +14,7 @@ class ThermometerSpec extends FlatSpec {
   }
 
   it should "return 'very precise' if the mean is within 0.5 and the std deviation is < 5" in {
-    assertResult("temp-1: very precise"){
+    assertResult(VeryPrecise()){
       val check = new Thermometer("temp-1", 70)
       check.addSample(70)
       check.addSample(66)
@@ -24,7 +24,7 @@ class ThermometerSpec extends FlatSpec {
   }
 
   it should "return pecise otherwise" in {
-    assertResult("temp-1: precise"){
+    assertResult(Precise()){
       val check = new Thermometer("temp-1", 70)
       check.addSample(0)
       check.addSample(1000)
